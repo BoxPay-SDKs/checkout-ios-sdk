d42792676ebaf128c4789009ac80a79e6c3a43ee
Ankush <ankush.kashyap@boxpay.tech>
2025-01-07 16:08:48 +0530

    Initial commit with main screen

---
+++ boxpay-ios-checkout/boxpay_ios_checkout.h
@@ -0,0 +1,18 @@
+//
+//  boxpay_ios_checkout.h
+//  boxpay-ios-checkout
+//
+//  Created by ankush on 30/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for boxpay_ios_checkout.
+FOUNDATION_EXPORT double boxpay_ios_checkoutVersionNumber;
+
+//! Project version string for boxpay_ios_checkout.
+FOUNDATION_EXPORT const unsigned char boxpay_ios_checkoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <boxpay_ios_checkout/PublicHeader.h>
+
+
