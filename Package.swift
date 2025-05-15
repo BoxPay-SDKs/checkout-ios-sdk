bde7d15f8e4cf6b10abbb5cb8ed76ba609b113b6
ishika bansal <ishika.bansal@boxpay.tech>
2025-05-15 10:09:08 +0530

    macOS version added

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
     name: "ios-checkout-sdk",
     platforms: [
-            .iOS(.v14)
-        ],
+        .macOS(.v10_15),
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
